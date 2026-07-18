83a4ecb4a58cbd3b9f2cf32dfa6ec90e008c0329
SiwarBouali <149542763+siwaresprit@users.noreply.github.com>
2026-07-18 20:22:09 +0100

    minio

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -62,7 +62,15 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    default="localhost,127.0.0.1",
+    cast=lambda value: [
+        item.strip()
+        for item in value.split(",")
+        if item.strip()
+    ],
+)
 
 
 # Application definition
