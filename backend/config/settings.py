74d58f0fe573a9c174013d1fb20f4ce24f95dd1c
SiwarBouali <149542763+siwaresprit@users.noreply.github.com>
2026-07-20 10:38:54 +0100

    minio

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -137,16 +137,35 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-#DATABASES = {
- #   "default": {
-  #      "ENGINE": "django.db.backends.postgresql",
-  #      "NAME": config("DB_NAME"),
-  #      "USER": config("DB_USER"),
-     #   "PASSWORD": config("DB_PASSWORD"),
-   #     "HOST": config("DB_HOST", default="localhost"),
-   #     "PORT": config("DB_PORT", default="5432"),
-   # }
-#}
+DATABASES = {
+    "default": {
+        "ENGINE": os.getenv(
+            "DB_ENGINE",
+            "django.db.backends.postgresql",
+        ),
+        "NAME": os.getenv(
+            "DB_NAME",
+            "esg_platform",
+        ),
+        "USER": os.getenv(
+            "DB_USER",
+            "esg_user",
+        ),
+        "PASSWORD": os.getenv(
+            "DB_PASSWORD",
+            "",
+        ),
+        "HOST": os.getenv(
+            "DB_HOST",
+            "127.0.0.1",
+        ),
+        "PORT": os.getenv(
+            "DB_PORT",
+            "5432",
+        ),
+        "CONN_MAX_AGE": 60,
+    }
+}
 
 
 DATABASES = {
